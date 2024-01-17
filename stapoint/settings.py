1edad517bd0de4037db099c62eec81837eff5d37
kingbonwa <kingbonwa@hotmail.co.za>
2024-01-17 15:49:09 +0200

    "all configs finished"

--- stapoint/settings.py
+++ stapoint/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICfILES_FINDERS = ('compressor.finders.CompressorFinder',)
